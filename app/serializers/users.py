3ef50edf7e811affa5e518c8d35a7acba8a7dc2a
SANTHOSHNS001 <santhoshnarayanan932000@gmail.com>
2025-09-11 16:25:27 +0530

    Register user function

--- app/serializers/users.py
+++ app/serializers/users.py
@@ -1,10 +1,32 @@
 from rest_framework import serializers
+from app.models.device_model import UserDevices, UserLocation
 from app.models.user_model import CustomUser
 
  
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserLocation
+        fields = [
+            "id",
+            "user",
+            "latitude",
+            "longitude",
+        ]
+        
+class DevicesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDevices
+        fields = [
+            "id",
+            "user",
+            "device_name",
+            "finger_print",
+        ]
 
 
 class UserSerializer(serializers.ModelSerializer):
+    location = serializers.SerializerMethodField()
+    device = serializers.SerializerMethodField()
 
     class Meta:
         model = CustomUser
@@ -13,8 +35,15 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
             "name",
             "mobile",
-            # "department_name",
-            # "role_name",
+            "location",  
+            "device"     
         ]
+    def get_location(self, obj):
+        last = obj.user_location.order_by("-id").first()
+        return LocationSerializer(last).data if last else None
+
+    def get_device(self, obj):
+        last = obj.user_device.order_by("-id").first()
+        return DevicesSerializer(last).data if last else None
 
  
